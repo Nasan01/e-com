7287c1d419d7ba96063203c9d8dbe4df19788d51
Avonasandratra Faustin RAMAROSON <avonasandratra2003@gmail.com>
2022-11-18 12:07:59 +0300

    signout feat and a temp dashboard for registered user

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,6 +1,13 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/inertia-vue3';
+import { Link, useForm } from '@inertiajs/inertia-vue3';
+
+const formLogout = useForm();
+
+const logout = () => {
+    formLogout.post(route('logout'));
+}
+
 </script>
 
 <template>
@@ -17,11 +24,24 @@ import { Link } from '@inertiajs/inertia-vue3';
                         <Link :href="route('home')">Home</Link>
                     </li>
                     <li>
-                        <Link :href="route('register')">Signup</Link>
+                        <Link href="#">Shop</Link>
                     </li>
-                    <li>
-                        <Link :href="route('login')">Signin</Link>
-                    </li>
+                    <template v-if="$page.props.auth.user">
+                        <li>
+                            <Link href="#">Dashboard</Link>
+                        </li>
+                        <li>
+                            <a @click="logout">Signout</a>
+                        </li>
+                    </template>
+                    <template v-else>
+                        <li>
+                            <Link :href="route('register')">Signup</Link>
+                        </li>
+                        <li>
+                            <Link :href="route('login')">Signin</Link>
+                        </li>
+                    </template>
                     <li><a class="btn btn-primary text-white text-sm px-3"><i class="fa fa-cart-shopping"></i> Cart</a></li>
                 </ul>
             </div>
